86a78c08d5b0065ba55e1df401cb855175e6ecde
nana <giannamarins@gmail.com>
2022-09-26 20:53:26 -0300

    adicionado arquivos do projeto

---
+++ script.js
@@ -0,0 +1,8 @@
+var listaAnime = [
+
+    "https://i.pinimg.com/originals/63/33/fd/6333fdb09959a558c4395019c8d117e2.jpg", "https://cdn11.bigcommerce.com/s-b72t4x/images/stencil/1280x1280/products/11169/20874/24_1075_Naruto_Group__97803.1624823918.jpg?c=2", "https://m.media-amazon.com/images/I/71V9XVhU0QL._AC_SX466_.jpg", "https://ptanime.com/wp-content/uploads/2022/05/One-Piece-anime-zou-arc-poster-promo.jpeg", "https://static.displate.com/857x1200/displate/2022-05-20/4e8bb552a604c9ceb815b429dd7c5856_34a061881e423ee08e95eb892e6b57f8.jpg", "https://m.media-amazon.com/images/I/71IrbfU+tyS._AC_SY679_.jpg"
+];
+
+for (var i = 0; i < listaAnime.length; i++) {
+    document.write("<img src=" + listaAnime[i] + "> ");
+}
